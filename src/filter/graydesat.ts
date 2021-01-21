8507a0d539dd539634ca98a5260557535985990d
liaorui <rui.liao@videt.cn>
2021-01-21 15:14:34 +0800

    灰度最大最小、灰度去饱和

---
+++ src/filter/graydesat.ts
@@ -0,0 +1,14 @@
+/**
+ * @description 灰度去饱和
+ * @param       {ImageData} imgData
+ * @return      {ImageData}
+ */
+function graydesat(imgData: ImageData): ImageData {
+    const data = imgData.data;
+    for (let i = 0, n = data.length; i < n; i += 4) {
+        const gray = (Math.max(data[i], data[i + 1], data[i + 2]), Math.min(data[i], data[i + 1], data[i + 2])) / 2;
+        data[i + 2] = data[i + 1] = data[i] = gray;
+    }
+    return imgData;
+}
+export default graydesat;
