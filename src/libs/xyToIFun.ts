795813f71569fdb51d250b85190b07c9beb1a36a
liaorui <rui.liao@videt.cn>
2021-01-21 14:57:15 +0800

    将(x,y)的坐标转为单维的i

---
+++ src/libs/xyToIFun.ts
@@ -0,0 +1,13 @@
+/** 
+ * @description 将(x,y)的坐标转为单维的i
+ * @param       {Number}    width
+ * @return      {Function}
+ */
+function xyToIFun(width: number): Function {
+    return function (x: number, y: number, z: number): number {
+        z = z || 0;
+        return (y * width + x) * 4 + z;
+    };
+}
+
+export default xyToIFun;
