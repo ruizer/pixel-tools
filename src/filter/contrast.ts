163cde70700047c98e59fc2f1022868323cd4669
liaorui <rui.liao@videt.cn>
2021-01-21 14:58:55 +0800

    对比度

---
+++ src/filter/contrast.ts
@@ -0,0 +1,18 @@
+/**
+ * @description 对比度
+ * @param       {Number}    contrast 0默认，-1时呈灰色
+ * @return      {ImageData}
+ */
+
+function contrast(imgData: ImageData, contrast: number = 1): ImageData {
+    const data = imgData.data;
+    const colorDataArrLen = data.length;
+    for (let i = 0; i < colorDataArrLen; i += 4) {
+        for (let j = 0; j < 3; j++) {
+            data[i + j] += (data[i + j] - 127.5) * contrast;
+        }
+    }
+    return imgData;
+}
+
+export default contrast;
