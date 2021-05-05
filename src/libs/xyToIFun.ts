d9b2edd8911f69231a33c7a6c0d80ab781a53124
liaorui <rui.liao@videt.cn>
2021-05-05 20:33:45 +0800

    类型注解

--- src/libs/xyToIFun.ts
+++ src/libs/xyToIFun.ts
@@ -1,9 +1,9 @@
-/** 
+/**
  * @description 将(x,y)的坐标转为单维的i
  * @param       {Number}    width
  * @return      {Function}
  */
-function xyToIFun(width: number): Function {
+function xyToIFun(width: number): (x: number, y: number, z: number) => number {
     return function (x: number, y: number, z: number): number {
         z = z || 0;
         return (y * width + x) * 4 + z;
